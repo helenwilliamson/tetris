3d4fc0dca93150f6bff0f84650e0569484a0aa2f
Helen Williamson <helen.williamson@acuris.com>
2018-05-31 13:47:32 +0100

    Adding pieces which stops when game full

--- src/tetris/core.cljs
+++ src/tetris/core.cljs
@@ -5,9 +5,9 @@
 
 (def height 200)
 
-(defonce app-state (atom [{:id 3 :x 20 :y 100 :height 20 :width 100 :colour "red"}
+(defonce app-state (atom [{:id 1 :x 20 :y 100 :height 20 :width 100 :colour "red"}
                           {:id 2 :x 0 :y 40 :height 20 :width 100 :colour "blue"}
-                          {:id 1 :x 100 :y 0 :height 20 :width 100 :colour "green"}]))
+                          {:id 3 :x 100 :y 0 :height 20 :width 100 :colour "green"}]))
 
 (defn overlaps
   [first second]
@@ -28,30 +28,40 @@
   [state]
   (reduce (fn 
             [current-state piece]
-            ;(println "piece" piece)
             (let [new-piece (assoc piece :y (+ 5 (:y piece)))
                   new-state (conj current-state new-piece)]
-              ;(println "new-piece" new-piece)
-              ;(println "new-state" new-state)
               (if (is-valid-world current-state new-piece)
                 new-state
                 (conj current-state piece))))
           [] state))
 
-(defonce gravity (js/setInterval #(swap! app-state drop-pieces) 300))
+(defonce adder-interval (atom 0))
+
+(defn add-piece
+  [state]
+  (let [new-piece {:id (count state) :x 50 :y 0 :height 20 :width 100 :colour "red"}]
+    (if (is-valid-world state new-piece)
+      (conj state new-piece)
+     (do
+       (js/clearInterval @adder-interval)
+       state))))
+
+(defonce gravity (js/setInterval #(swap! app-state drop-pieces) 500))
+
+(defonce adder (reset! adder-interval (js/setInterval #(swap! app-state add-piece), 5000)))
 
 (defn horizontal-rectangle
   [{id :id x :x y :y width :width height :height colour :colour}]
   [:rect {:width width :height height :x x :y y :key id :fill colour}])
 
-(defn hello-world []
+(defn tetris []
   [:div
    [:h3 "Tetris"]
    [:svg {:width 200 :height height :style {:background "black" :border "1px solid"}}
     (for [piece @app-state]
       (horizontal-rectangle piece))]])
 
-(reagent/render-component [hello-world]
+(reagent/render-component [tetris]
                           (. js/document (getElementById "app")))
 (swap! app-state drop-pieces)
 
