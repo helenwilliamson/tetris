e8ad955b8b1f3afe378c3ee2ff9beaec0f21122f
Helen Williamson <helen.williamson@acuris.com>
2018-05-31 13:30:20 +0100

    Removed unused method and coloured blocks

--- src/tetris/core.cljs
+++ src/tetris/core.cljs
@@ -5,13 +5,9 @@
 
 (def height 200)
 
-(defonce app-state (atom [{:id 3 :x 20 :y 100 :height 20 :width 100}
-                          {:id 2 :x 0 :y 40 :height 20 :width 100}
-                          {:id 1 :x 100 :y 0 :height 20 :width 100}]))
-
-(defn add-gravity 
-  [state] 
-  (map #(assoc % :y (+ 5 (:y %))) state))
+(defonce app-state (atom [{:id 3 :x 20 :y 100 :height 20 :width 100 :colour "red"}
+                          {:id 2 :x 0 :y 40 :height 20 :width 100 :colour "blue"}
+                          {:id 1 :x 100 :y 0 :height 20 :width 100 :colour "green"}]))
 
 (defn overlaps
   [first second]
@@ -45,13 +41,13 @@
 (defonce gravity (js/setInterval #(swap! app-state drop-pieces) 300))
 
 (defn horizontal-rectangle
-  [{id :id x :x y :y width :width height :height}]
-  [:rect {:width width :height height :x x :y y :key id}])
+  [{id :id x :x y :y width :width height :height colour :colour}]
+  [:rect {:width width :height height :x x :y y :key id :fill colour}])
 
 (defn hello-world []
   [:div
    [:h3 "Tetris"]
-   [:svg {:width 200 :height height}
+   [:svg {:width 200 :height height :style {:background "black" :border "1px solid"}}
     (for [piece @app-state]
       (horizontal-rectangle piece))]])
 
