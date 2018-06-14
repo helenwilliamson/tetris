f275bfa126968ef57b70902ba08c388bec13d28f
Helen Williamson <helen.williamson@acuris.com>
2018-06-14 09:38:40 +0100

    Converted to model each square of a shape

--- src/tetris/core.cljs
+++ src/tetris/core.cljs
@@ -4,40 +4,45 @@
 
 (enable-console-print!)
 
-(def height 500)
-(def width 500)
+(def height 300)
+(def width 200)
 
-(def colours ["red" "lime" "yellow" "aqua" "fuchsia"])
+(def colours ["red" "lime" "yellow" "aqua" "fuchsia" "blue"])
 (def piece-type [:rectangle :square])
 
 (defonce game (atom []))
 (defonce status (atom ""))
 (defonce game-updater (atom 0))
 
+(defn update-values
+  [m f & args]
+  (reduce (fn [r [k v]] (assoc r k (apply f v args))) {} m))
+
 (defn overlaps
-  [first second]
-  (let [first-bottom-right (hash-map :x (+ (:width first) (:x first)) :y (+ (:height first) (:y first)))
-        second-bottom-right (hash-map :x (+ (:width second) (:x second)) :y (+ (:height second) (:y second)))]
-    (not (or (>= (:x first) (:x second-bottom-right))
-             (>= (:x second) (:x first-bottom-right))
-             (>= (:y first) (:y second-bottom-right))
-             (>= (:y second) (:y first-bottom-right))))))
+  [{first-blocks :blocks} {second-blocks :blocks}]
+
+  (let [first-values (set (vals first-blocks))
+        second-values (set (vals second-blocks))]
+    (println "first values" first-values)
+    (println "second values" second-values)
+    (some #(contains? second-values %1) first-values)))
 
 (defn is-valid-world
   [state new-piece]
-  (let [at-bottom-edge (< height (+ (:height new-piece) (:y new-piece)))
-        at-left-edge (< (:x new-piece) 0)
-        at-right-edge (> (+ (:width new-piece) (:x new-piece)) width)
+  (let [at-bottom-edge (some #(< height (+ 25 (:y %1))) (vals (:blocks new-piece)))
+        at-left-edge (some #(< (:x %1) 0) (vals (:blocks new-piece)))
+        at-right-edge (some #(> (+ 25 (:x %1)) width) (vals (:blocks new-piece)))
         pieces-overlap (not= 0 (count (filter #(overlaps new-piece %1) state)))]
     (not (or at-bottom-edge at-left-edge at-right-edge pieces-overlap))))
 
 (defn apply-direction
   [piece direction]
-  (cond
-   (= direction :down) (assoc piece :y (+ (:y piece) 25))
-   (= direction :left) (assoc piece :x (- (:x piece) 25))
-   (= direction :right) (assoc piece :x (+ (:x piece) 25))
-   :else piece))
+  (let [blocks (:blocks piece)]
+    (cond
+     (= direction :down) (assoc piece :blocks (update-values blocks (fn [coord] (assoc coord :y (+ (:y coord) 25)))))
+     (= direction :left) (assoc piece :blocks (update-values blocks (fn [coord] (assoc coord :x (- (:x coord) 25)))))
+     (= direction :right) (assoc piece :blocks (update-values blocks (fn [coord] (assoc coord :x (+ (:x coord) 25)))))
+     :else piece)))
 
 (defn move-piece
   [state direction]
@@ -66,8 +71,8 @@
 (defn make-piece
   [id x colour piece-type]
   (cond
-   (= piece-type :rectangle) {:id id :x x :y 0 :height 25 :width 100 :colour colour}
-   (= piece-type :square) {:id id :x x :y 0 :height 50 :width 50 :colour colour}))
+   (= piece-type :rectangle) {:x x :y 0 :height 25 :width 100 :colour colour :blocks {id {:x x :y 0} (+ 1 id) {:x (+ 25 x) :y 0} (+ 2 id) {:x (+ 50 x) :y 0} (+ 3 id) {:x (+ 75 x) :y 0}}}
+   (= piece-type :square) {:x x :y 0 :height 50 :width 50 :colour colour :blocks {id {:x x :y 0} (+ 1 id) {:x (+ 25 x) :y 0} (+ 2 id) {:x x :y 25} (+ 3 id) {:x (+ 25 x) :y 25}}}))
 
 (defn add-piece
   [state]
@@ -75,13 +80,13 @@
         random-piece-type (select-random piece-type)
         possible-x (vec (range 0 (- width 100) 25))
         random-x (select-random possible-x)
-        new-piece (make-piece (count state) random-x random-colour random-piece-type)]
+        new-piece (make-piece (* 4 (count state)) random-x random-colour random-piece-type)]
     (if (is-valid-world state new-piece)
       (conj state new-piece)
-     (do
-       (js/clearInterval @game-updater)
-       (reset! status "Game Over")
-       state))))
+      (do
+        (js/clearInterval @game-updater)
+        (reset! status "Game Over")
+        state))))
 
 (defn update-game
   [state]
@@ -101,16 +106,19 @@
 (defonce gravity (reset! game-updater (js/setInterval #(swap! game update-game) 500)))
 (defonce adder (swap! game add-piece))
 
-(defn horizontal-rectangle
-  [{id :id x :x y :y width :width height :height colour :colour}]
-  [:rect {:width width :height height :x x :y y :key id :fill colour :stroke "black" :stroke-width 1}])
+(defn rectangle
+  [block colour]
+  (let [id (first block)
+        {x :x y :y} (nth block 1)]
+    [:rect {:width 25 :height 25 :x x :y y :key id :fill colour :stroke "black" :stroke-width 1}]))
 
 (defn tetris []
   [:div
    [:h3 "Tetris"]
    [:svg {:width width :height height :style {:border "1px solid"}}
     (for [piece @game]
-      (horizontal-rectangle piece))]
+      (for [block (:blocks piece)]
+        (rectangle block (:colour piece))))]
    [:p
     [:strong @status]]])
 
